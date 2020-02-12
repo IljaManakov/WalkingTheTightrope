dba5cacd7b40a2e366f11ee015e0c983d8454c6f
IljaManakov <ilja.manakov@med.uni-muenchen.de>
2020-02-12 12:23:49 +0100

    added cmd support

--- train.py
+++ train.py
@@ -1,6 +1,7 @@
 from trainer import Trainer, events, Config
 from trainer.handlers import EventSave
 from torch.utils.data import DataLoader
+import argparse
 
 
 def run_training(config):
@@ -12,13 +13,11 @@ def run_training(config):
 
     # validation
     validation_loader = DataLoader(config.DATASET(**config.validationset), **config.dataloader)
-    sample = next(iter(validation_loader))
-    sample = [s[0:config.n_samples] for s in sample]
 
-    # setup monitoring
+    # setup events
     # trainer.register_event_handler(events.EACH_EPOCH, trainer, name='sample', sample=sample)
-    trainer.register_event_handler(events.EACH_STEP, trainer.validate, dataloader=validation_loader, interval=config.save_interval//10)
-    trainer.register_event_handler(events.EACH_STEP, EventSave(), interval=config.save_interval)
+    trainer.register_event_handler(events.EACH_EPOCH, trainer.validate, dataloader=validation_loader)
+    # trainer.register_event_handler(events.EACH_STEP, EventSave(), interval=config.save_interval)
 
     # train classifier
     if hasattr(config, 'n_steps'):
@@ -29,8 +28,44 @@ def run_training(config):
 
 if __name__ == '__main__':
 
-    config_file = '/home/config.py'
+    # parse cmd arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--config', '-c', choices=['pokemon', 'celeba', 'stl-10', 'scaling_celeba'], nargs=1, type=str,
+                        required=True, metavar='config name', help='configuration file for the AE training')
+    parser.add_argument('--bottleneck_size', '-s', choices=['small', 'mid', 'large'], nargs=1, type=str, metavar='size string',
+                        help='choose bottleneck size (height x width) setting of the AE (overrides config)')
+    parser.add_argument('--channels', '-ch', type=int, nargs=1, metavar='n_channels',
+                        help='choose number of channels in the bottleneck (overrides config)')
+    parser.add_argument('--seed', type=int, nargs=1, metavar='int',
+                        help='seed for the model initialization and training run')
+    parser.add_argument('--cpu', type=int, nargs=0,
+                        help='run training on cpu')
+    parser.add_argument('--epochs', type=int, nargs=1, metavar='int',
+                        help='choose number of epochs for training (overrides config)')
+    parser.add_argument('--steps', type=int, nargs=1, metavar='int',
+                        help='choose number of steps for training (overrides config and overrules epochs)')
+    args = parser.parse_args()
+
+    # load config file
+    config_file = f'configs/{args.config}_config.py'
     config = Config.from_file(config_file)
+
+    # modify config based on cmd arguments
+    if hasattr(args, 'bottleneck_size'):
+        strides_to_remove = {'small': 0, 'mid': 1, 'large': 2}
+        config.superfluous_strides = strides_to_remove[args.bottleneck_size]
+    if hasattr(args, 'channels'):
+        config.model['channels'][-1] = args.channels
+    if hasattr(args, 'seed'):
+        config.seed = args.seed
+    if hasattr(args, 'cpu'):
+        config.cuda = False
+    if hasattr(args, 'epochs'):
+        config.n_epochs = args.epochs
+    if hasattr(args, 'steps'):
+        setattr(config, 'steps', args.steps)
+
+    # commence training
     run_training(config)
 
 
